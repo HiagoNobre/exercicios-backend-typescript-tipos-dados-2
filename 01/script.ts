4c3e8f51246d466b5ebbc927831458b16aba74ae
nobreh <nobrehiago21@hotmail.com>
2023-05-19 21:12:08 -0300

    exportando funcoes e retirando funcoes de testes

--- 01/script.ts
+++ 01/script.ts
@@ -2,7 +2,7 @@ import fs from "node:fs/promises";
 
 const dbFilePath: string = "./db.json";
 
-const readBdFile = async (): Promise<string> => {
+export const readBdFile = async (): Promise<string> => {
   try {
     const fileBuffer: Buffer = await fs.readFile(dbFilePath);
 
@@ -13,16 +13,6 @@ const readBdFile = async (): Promise<string> => {
   }
 }
 
-const writeToDbFile = async (content: any): Promise<void> => {
+export const writeToDbFile = async (content: any): Promise<void> => {
   await fs.writeFile(dbFilePath, JSON.stringify(content));
 }
-
-const writeRead = async (content: any): Promise<void> => {
-  await writeToDbFile(content);
-
-  const textFile: string = await readBdFile();
-
-  console.log(textFile);
-}
-
-writeRead("Do javascript ao typescript");
